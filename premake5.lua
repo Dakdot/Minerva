04454e9039c24c0eef5212069c73140e2d7993fb
Thiago Andrade <tandrade201@outlook.com>
2019-06-15 18:14:13 -0400

    Set up Premake

---
+++ premake5.lua
@@ -0,0 +1,98 @@
+workspace "Rambutan"
+    architecture "x64"
+
+    configurations {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Rambutan"
+    location "Rambutan"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.hpp",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs {
+        "Rambutan/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines {
+            "RB_PLATFORM_WINDOWS",
+            "RB_BUILD_DLL"
+        }
+
+        postbuildcommands {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+        }
+
+    filter "configurations:Debug"
+        defines "RB_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "RB_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "RB_DIST"
+        optimize "On"
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.hpp",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs {
+        "Rambutan/vendor/spdlog/include",
+        "Rambutan/src"
+    }
+
+    links {
+        "Rambutan"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines {
+            "RB_PLATFORM_WINDOWS"
+        }
+
+    filter "configurations:Debug"
+        defines "RB_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "RB_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "RB_DIST"
+        optimize "On"
